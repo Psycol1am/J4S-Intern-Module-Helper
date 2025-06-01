173fdb5bc9d303475dc5454708ca9f75ac83fe68
Liam-James-Ford <19067511@stu.mmu.ac.uk>
2025-06-02 00:40:12 +0100

    Initial commit added splitting of allstudents.csv into multiple files based on a specefied size(100)

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ from tkinter import *
 
 df = pd.DataFrame(pd.read_csv('allstudents.csv'))
 
+split_size = 100
+split = [df.iloc[i:i + split_size] for i in range(0, len(df), split_size)]
+
+for i  in range(len(split)):
+    split[i].to_csv(f'allstudents{i+1}.csv')
+
+
+
+
 
 root = tk.Tk()
 root.state('zoomed')
@@ -15,10 +24,10 @@ frame.pack(expand=True, fill='both')
 text = Text(frame, wrap='none')
 
 
-yscrollbar = Scrollbar(frame, orient='vertical', command=text.yview)
-yscrollbar.pack(side='right', fill='y')
+yscroll = Scrollbar(frame, orient='vertical', command=text.yview)
+yscroll.pack(side='right', fill='y')
 
-text.configure(yscrollcommand=yscrollbar.set)
+text.configure(yscrollcommand=yscroll.set)
 text.insert(END, df.to_string())
 text.pack(expand=True, fill='both')
 
